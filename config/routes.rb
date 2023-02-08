d822b8c0e56e4667402351259ae699b7d27f9293
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2023-02-08 06:10:09 +0100

    Set up configuration in devis.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   namespace :api do
     namespace :v1 do
       resources :products do
@@ -7,5 +10,5 @@ Rails.application.routes.draw do
       end
     end
   end
-
+root 'api/v1/products#index'
 end
